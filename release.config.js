7c5c93c837af06069dc91aa3d02008903e1c8fb2
Szymon Nowicki <sznowicki@eskalacja.com>
2020-09-20 19:20:09 +0200

    chore: preparations

---
+++ release.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  branches: [
+    'master',
+    {
+      name: 'rc',
+      prerelease: true,
+    },
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    ['@semantic-release/release-notes-generator', {
+      preset: 'conventionalcommits',
+    }],
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md',
+    }],
+    ['@semantic-release/npm'],
+    [
+      '@semantic-release/git', {
+        assets: [
+          'CHANGELOG.md',
+          'package.json',
+        ],
+      },
+    ],
+  ],
+};
